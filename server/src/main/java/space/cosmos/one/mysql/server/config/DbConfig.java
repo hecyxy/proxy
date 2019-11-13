b79eae128daca4f54d906a36c996210c16a24be7
hecy <hecy.hecy@lieluobo.com>
2019-11-13 10:08:53 +0800

    add private config

---
+++ server/src/main/java/space/cosmos/one/mysql/server/config/DbConfig.java
@@ -0,0 +1,6 @@
+package space.cosmos.one.mysql.server.config;
+
+public class DbConfig {
+    public static final String dbUrl = "*";
+    public static final String dbUrl2 = "*";
+}
--- server/src/main/java/space/cosmos/one/mysql/server/endpoint/InstanceServiceGrpcImpl.java
+++ server/src/main/java/space/cosmos/one/mysql/server/endpoint/InstanceServiceGrpcImpl.java
@@ -7,6 +7,9 @@ import space.cosmos.one.proxy.mysql.ListInstance;
 import java.util.Arrays;
 import java.util.List;
 
+import static space.cosmos.one.mysql.server.config.DbConfig.dbUrl;
+import static space.cosmos.one.mysql.server.config.DbConfig.dbUrl2;
+
 public class InstanceServiceGrpcImpl extends InstanceServiceGrpc.InstanceServiceImplBase {
 
     @Override
@@ -14,8 +17,9 @@ public class InstanceServiceGrpcImpl extends InstanceServiceGrpc.InstanceService
 
         return new StreamObserver<ListInstance.Request>() {
             List<ListInstance.Response.Instance> list = Arrays.asList(
-                    ListInstance.Response.Instance.newBuilder().setHost("172.16.52.81").setId(1).setPort(3306).build());
-//                    ListInstance.Response.Instance.newBuilder().setHost("192.168.10.12").setId(2).setPort(1026).build(),
+                    //import yours dburl and port
+                    ListInstance.Response.Instance.newBuilder().setHost(dbUrl).setId(1).setPort(3306).build(),
+                    ListInstance.Response.Instance.newBuilder().setHost(dbUrl2).setId(2).setPort(3306).build());
 //                    ListInstance.Response.Instance.newBuilder().setHost("192.168.10.12").setId(3).setPort(1027).build());
 
 
