ded0c46bb5b5f44abafd233729f6792a233a4a79
hecyxy <hcy_xy@qq.com>
2019-11-29 21:24:10 +0800

    1.jmh plugin 2.annotation processing 3.jmh

--- binlog-sync/src/test/java/space/cosmos/one/binlog/TestBuffer.java
+++ binlog-sync/src/test/java/space/cosmos/one/binlog/TestBuffer.java
@@ -1,7 +1,16 @@
 package space.cosmos.one.binlog;
 
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.concurrent.TimeUnit;
+
+@State(Scope.Benchmark)
 public class TestBuffer {
 
     private static int c;
@@ -17,7 +26,7 @@ public class TestBuffer {
         }
     }
 
-    @Test
+//    @Test
     public void test() {
         int a = 3;
         sys(3);
@@ -25,6 +34,7 @@ public class TestBuffer {
         String b = "hcy";
         sys(b);
         System.out.println(b);
+        Assert.assertTrue(b == "ff");
     }
 
     void sys(int b) {
@@ -34,4 +44,22 @@ public class TestBuffer {
     void sys(String str) {
         str = "aaa";
     }
+
+    @Benchmark
+    @BenchmarkMode(Mode.All)
+    @OutputTimeUnit(TimeUnit.MILLISECONDS)
+    public String concat() {
+        String a = "a";
+        String b = "b";
+        String s = a + b;
+        return s;
+    }
+
+//    @Test
+    public static void main(String[] args) throws RunnerException {
+        Options opt = new OptionsBuilder().include(TestBuffer.class.getSimpleName())
+                .measurementIterations(5).build();
+        new Runner(opt).run();
+    }
+
 }
